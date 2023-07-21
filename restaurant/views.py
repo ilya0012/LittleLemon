3908e9d1780d1d48b0907d183c9681e421406d07
ReeperV2 <ilya00chistov@gmail.com>
2023-07-21 06:16:33 -0700

    bookingView added + url with router

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-#from .models import Menu
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from .serializers import BookingSerializer, MenuSerializer
+from .models import Booking, Menu
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
