1c2a0ede0dba24ef2321e370127b1070734ec2f7
ReeperV2 <ilya00chistov@gmail.com>
2023-07-22 11:27:47 -0700

    added test + menu

--- restaurant/views.py
+++ restaurant/views.py
@@ -12,10 +12,12 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
